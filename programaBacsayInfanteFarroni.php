036930c68073eeaf2498dfc5242623f78d779c63
MGIC12 <mager.infante@gmail.com>
2023-10-27 17:03:26 -0300

    Implementación de palabras

--- programaBacsayInfanteFarroni.php
+++ programaBacsayInfanteFarroni.php
@@ -27,8 +27,8 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "ARBOL", "HOJAS", "ACERO", "ACIDO", "ARROZ"
     ];
 
     return ($coleccionPalabras);
